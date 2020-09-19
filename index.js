2cd1f1c9b212505e84fae7c93737ebfa8d8de5b4
Yojan Stiben Hungría <yshungria@unal.edu.co>
2020-09-19 03:10:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+https://swapi.dev/
+
+https://pokeapi.co/
+
+https://formsubmit.co/
+Una API es una interfaz cuya finalidad es comunicar aplicaciones entre sí.
+Cuando se construye un sitio web, es muy común utilizar distintas APIS que han sido desarrolladas por otras personas/empresas, esto ayuda muchísimo para no perder tiempo “reinventando la rueda”.
+Por ejemplo, existe una API llamada formsubmit.co, que te permite enviar datos desde una pagina a un correo eléctronico sólo poniendo tu dirección de correo en un link, esto es muy útil porque sino tendrías que crear toda una aplicación de servidor para poder enviar correos desde tu pagina a tu cuenta de correo.
+Ahora, swapi.co (ahora swapi.dev) es una API REST, las API REST son servicios que te brindan datos de algo en específico. Es muy común utilizar API REST en desarrollo web ya que al programar una pagina NO tienes acceso a una base de datos (no directamente), pero esta API REST sí puede comunicarse con una base de datos, entonces estaría actuando como intermediario para brindarte los datos que deseas. swapi.dev te brinda información sobre Star wars, para que puedas utilizar dicha información en tus aplicaciones, ya sea para practicar o profesionalmente. Existen muchas API REST gratuitas en internet, las más populares son:
+-Pokeapi (te brinda datos de pokemon, obviamente)
+-The Movie Api (Te brinda datos de películas, estrenos, repartos, sinopsis, banners, etc)
+-etc
+
+Las API REST son buenísimas porque te brindan la información y tú sólo te encargas de manipularla y mostrarlas.
+Recomiendo ver algún video de Como hacer un pokedex con Javascript, ahí consumen los datos de la pokeapi (cualquier tutorial sirve).
+
+
